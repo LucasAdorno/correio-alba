4fc3c1968201b832ed7dac4722e2cf1c6b848ffd
Igor Baldoino <igortthq@gmail.com>
2021-01-07 23:04:41 -0300

    update

---
+++ src/components/HistoryChart/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 98%;
+  height: 400px;
+`;
--- src/pages/Homepage/styles.ts
+++ src/pages/Homepage/styles.ts
@@ -9,7 +9,6 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
 `;
 
 export const Main = styled.div`

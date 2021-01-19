3fa9f5b9595e260c5e23fc8d63172ece6f4bf962
LucasAdorno <lucasadorno2702@gmail.com>
2021-01-19 08:03:34 -0300

    [cod] inserindo campo de pesquisa

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const app = axios.create({ baseURL: "https://albaapi.herokuapp.com" });
-const app = axios.create({ baseURL: "http://localhost:8080" });
+const app = axios.create({ baseURL: "https://albaapi.herokuapp.com" });
+// const app = axios.create({ baseURL: "http://localhost:8080" });
 
 
 export default app;
